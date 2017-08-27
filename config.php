5807aee9e46d4ca6a341802d833658072acaa5bb
lostcodder <lostcodder@gmail.com>
2017-08-27 10:33:11 +0500

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
-$access_token = "0b313d079390e08dca2335b025dee5fb5f71774f7db71111621fc21b43599b379ec18c2e094f0ac3b54aa";
-$group_id = "152196016";
+$access_token = "---Access token---";
+$group_id = "---Group id---";
 $change_interval = 30;
 
 $covers_dir = "cover";
